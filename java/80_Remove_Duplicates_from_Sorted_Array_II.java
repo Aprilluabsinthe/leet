dc805b033a4a0bcc112c0d41fedac4c5565ab9fc
Di Lu <dilu@andrew.cmu.edu>
2021-06-22 00:04:36 -0400

    arrays

---
+++ java/80_Remove_Duplicates_from_Sorted_Array_II.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0 ;
+        for(int num : nums){
+            if( i < 2 || num > nums[i-2]){
+                nums[i++] = num;
+            }
+        
+        }
+        return i;
+    }
+
+    public int removeDuplicates1(int[] nums) {
+        if(nums.length <= 2) return nums.length;
+        int index = 2;
+
+        for(int i = 2 ; i < nums.length ; i++){
+            if(nums[i] != nums[index-2]){
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+}
