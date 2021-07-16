dce2d9d388e7b87b61bce9cec128dfaef9d78ce1
Di Lu <dilu@andrew.cmu.edu>
2021-07-15 20:35:13 -0400

    binary search

--- python/33_Search_in_Rotated_Sorted_Array.py
+++ python/33_Search_in_Rotated_Sorted_Array.py
@@ -1,36 +1,11 @@
-class Solution {
-    public int search(int[] nums, int target) {
-        if(nums == null){
-            return -1;
-        }
-        if(nums[0] == target){
-            return 0;
-        }
-        if(nums.length == 1){
-            return -1;
-        }
-
-        int left = 0, right = nums.length-1;
-
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            if(nums[0] < nums[mid]){
-                if( nums[0] <= target && target < nums[mid]){
-                    right = mid+1;
-                }
-                else{
-                    left = mid-1;
-                }
-            }
-            else{
-                if( nums[mid] <= target && target < nums[right]){
-                    left = mid-1;
-                }
-                else{
-                    right = mid+1;
-                }
-            }
-        }
-        return -1;
-    }
-}
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        left, right = 0, len(nums)-1
+        while(left < right):
+            mid = left + (right-left)//2
+            if(nums[mid] > nums[mid+1]):
+                right = mid
+            else:
+                left = mid + 1
+        return left
--- python/74_Search_a_2D_Matrix.py
+++ python/74_Search_a_2D_Matrix.py
@@ -21,3 +21,15 @@ class Solution:
                 return findInRow(matrix[j],target)
         
         return False
+    
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        x,y = 0, n - 1
+        while x < m and y >= 0:
+            if matrix[x][y] > target:
+                y -= 1
+            elif matrix[x][y] < target:
+                x += 1
+            else:
+                return True
+        return False
