c0e9845c7dc85159ef464b7b0b837acc761319ad
Di Lu <dilu@andrew.cmu.edu>
2021-07-16 02:20:01 -0400

    manipuate special map

---
+++ python/49_Group_Anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if len(strs) == 1:
+            return [strs]
+        res = collections.defaultdict(list)
+
+        for s in strs:
+            res[ "".join(sorted(s)) ].append(s)
+        return list( res.values())
