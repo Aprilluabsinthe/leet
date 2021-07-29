378dbdc3da7454e3f682106e382bf56dcf78db4c
Di Lu <dilu@andrew.cmu.edu>
2021-07-29 00:50:43 -0400

    add bfs

--- python/130_Surrounded_Regions.py
+++ python/130_Surrounded_Regions.py
@@ -22,13 +22,32 @@ class Solution:
             for i in range(4):
                 dfs(x+dx[i],y+dy[i])
         
+        def bfs(x,y):
+            from collections import deque
+            queue = deque()
+            queue.append((x,y))
+            while queue:
+                # mark all egde 'O's as 'A'
+                (x,y) = queue.pop()
+                if(x < 0 or x >= row or y < 0 or y >= col):
+                    continue
+                if board[x][y] != 'O':
+                    continue
+
+                dx = [0,1,0,-1]
+                dy = [1,0,-1,0]
+                board[x][y] = 'A'
+
+                for i in range(4):
+                    queue.append((x+dx[i],y+dy[i]))
+        
         for i in range(row):
-            dfs(i,0)
-            dfs(i,col-1)
+            bfs(i,0)
+            bfs(i,col-1)
         
         for i in range(col):
-            dfs(0,i)
-            dfs(row-1,i)
+            bfs(0,i)
+            bfs(row-1,i)
         
         for x in range(row):
             for y in range(col):
@@ -36,9 +55,4 @@ class Solution:
                    board[x][y] = 'O'
                 elif board[x][y] == 'O':
                    board[x][y] = 'X'
-
-
-
-
-
         
