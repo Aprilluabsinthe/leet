988f4c4028dd19c9f593861e87f5e87f5d0e18e2
Di Lu <dilu@andrew.cmu.edu>
2021-08-10 16:21:15 -0700

    add bfs and dfs method

--- python/103_Binary_Tree_Zigzag_Level_Order_Traversal.py
+++ python/103_Binary_Tree_Zigzag_Level_Order_Traversal.py
@@ -21,4 +21,56 @@ class Solution:
             tranversal(root.right,level+1)
         tranversal(root,0)
         return res
+    
+    # DFS
+    def zigzagLevelOrderDFS(self, root: Optional[TreeNode]) -> List[List[int]]:
+        from collections import deque
+        if not root:
+            return root
+        res = []
+        
+        def dfs(node,level):
+            if level >= len(res):
+                res.append(deque([node.val]))
+            else:
+                if level % 2 == 0:
+                    # left->right, normal order
+                    res[level].append(node.val)
+                else:
+                    res[level].appendleft(node.val)
+            if node.left:
+                dfs(node.left,level+1)
+            if node.right:
+                dfs(node.right,level+1)
+        dfs(root,0)
+        return res
+        
+        
+    def zigzagLevelOrderBFS(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return root
+        
+        q = collections.deque()
+        ans = []
+        q.append(root)
+        flip = True
+        
+        while q:
+            level = collections.deque()
+            lenq = len(q)
+            i = 0
+            flip = not flip   # next level
+            while i < lenq:
+                curNode = q.popleft()
+                if curNode.left:
+                    q.append(curNode.left)
+                if curNode.right:
+                    q.append(curNode.right)
+                if not flip:
+                    level.append(curNode.val)
+                else:
+                    level.appendleft(curNode.val)
+                i += 1
+            ans.append(level)
+        return ans
 
