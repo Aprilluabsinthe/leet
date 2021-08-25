69b25d4090d1357d74950835710300f91ab5e9c1
Di Lu <dilu@andrew.cmu.edu>
2021-08-25 12:41:17 -0700

    Counter

---
+++ palindromeRearranging.py
@@ -0,0 +1,17 @@
+def palindromeRearranging(inputString):
+    n = len(inputString)
+    # even, can not have single elements
+    # odd, can and must have one single elements
+    
+    from collections import Counter
+    count = Counter(inputString)
+    
+    numOdd = 0
+    if n % 2 == 1:
+        numOdd = 1
+        
+    for c in count:
+        if count[c] % 2 == 1:
+            numOdd -= 1
+    
+    return numOdd == 0
