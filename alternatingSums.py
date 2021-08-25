c744657f0a34f016ff3ca4922435cdd668d73437
Di Lu <dilu@andrew.cmu.edu>
2021-08-25 10:51:53 -0700

    slices usage

---
+++ alternatingSums.py
@@ -0,0 +1,24 @@
+## Several people are standing in a row and need to be divided into two teams. The first person goes into team 1, the second goes into team 2, the third goes into team 1 again, the fourth into team 2, and so on.
+
+## You are given an array of positive integers - the weights of the people. Return an array of two integers, where the first element is the total weight of team 1, and the second element is the total weight of team 2 after the division is complete.
+
+## Example: For a = [50, 60, 60, 45, 70], the output should be alternatingSums(a) = [180, 105].
+
+def alternatingSums(a):
+    n = len(a)
+    flag = True
+    odd = 0
+    even = 0
+    for i in range(n):
+        if flag:
+            odd += a[i]
+        else:
+            even += a[i]
+        flag = not flag
+    return [odd, even]
+
+    # return [sum(a[::2]),sum(a[1::2])]
+        
+        
+        
+
