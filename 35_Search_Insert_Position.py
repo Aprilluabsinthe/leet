13eac9240c723eef48b24cbc44ff24ac780dbda9
Di Lu <dilu@andrew.cmu.edu>
2021-08-29 22:55:28 -0700

    binary search

---
+++ 35_Search_Insert_Position.py
@@ -0,0 +1,22 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        if nums[0] >= target:
+            return 0
+        if nums[-1] < target:
+            return n
+
+        left = 0
+        right = n
+
+        while left <= right:
+            mid = left + (right - left)//2
+            if nums[mid] == target:
+                return mid
+            else:
+                if nums[mid] > target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return left
+        
