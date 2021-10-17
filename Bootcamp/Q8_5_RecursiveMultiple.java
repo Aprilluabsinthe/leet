950fba2ddb57ab96babf08fec16a3b28a85f4b27
Di Lu <dilu@andrew.cmu.edu>
2021-10-17 11:26:22 -0700

    recursio muptiple

---
+++ Bootcamp/Q8_5_RecursiveMultiple.java
@@ -0,0 +1,20 @@
+public class Q8_5_RecursiveMultiple {
+    public int minProduct(int a , int b){
+        int bigger = a < b ? b : a;
+        int smaller = a < b ? a : b;
+        return minProductHelper(smaller, bigger);
+    }
+
+    public int minProductHelper(int smaller, int bigger){
+        if(smaller == 0) return 0;
+        else if(smaller == 1) return bigger;
+
+        int s = smaller >> 1;
+        int halfProd = minProductHelper(s, bigger);
+        if(smaller % 2 == 0){
+            return halfProd + halfProd;
+        }else{
+            return halfProd + halfProd + bigger;
+        }
+    }
+}
