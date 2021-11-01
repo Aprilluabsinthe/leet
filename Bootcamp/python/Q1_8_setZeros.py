cae785977258841d8546bdc3d577c594abc78829
Di Lu <dilu@andrew.cmu.edu>
2021-11-01 09:18:02 -0700

    cict python chapter1

---
+++ Bootcamp/python/Q1_8_setZeros.py
@@ -0,0 +1,31 @@
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        clearFirstRow = False
+        for i in range(n):
+            if(matrix[0][i] == 0):
+                clearFirstRow = True
+        
+        for i in range(1,m):
+            for j in range(n):
+                if(matrix[i][j] == 0):
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        
+        for i in range(1,m):
+            if(matrix[i][0] == 0):
+                for j in range(n):
+                    matrix[i][j] = 0
+                    
+        for j in range(n):
+            if(matrix[0][j] == 0):
+                for i in range(1,m):
+                    matrix[i][j] = 0
+                    
+        if clearFirstRow == True:
+            for i in range(n):
+                matrix[0][i] = 0
