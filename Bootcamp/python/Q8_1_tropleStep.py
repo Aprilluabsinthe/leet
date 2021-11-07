edb41ae2ffc847e41eff5adb92f281c9ca306f93
Di Lu <dilu@andrew.cmu.edu>
2021-11-07 07:44:30 -0800

    simple dp python

---
+++ Bootcamp/python/Q8_1_tropleStep.py
@@ -0,0 +1,12 @@
+def TripleHopRecursive(x, memo):
+    if x < 0:
+        return 0
+    memo[0] = 1
+    if x >= 1:
+        memo[1] = 1
+    if x >= 2:
+        memo[2] = memo[1] + memo[0]
+    if x > 2:
+        for i in range(3, x + 1):
+            memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3]
+    return memo[x]
